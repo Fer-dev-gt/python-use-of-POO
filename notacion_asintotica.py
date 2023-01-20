90a2578197fa0c819b9d523a35924b84de838fc6
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-01-19 19:39:22 -0600

    Small changes to improve readibility

--- notacion_asintotica.py
+++ notacion_asintotica.py
@@ -2,12 +2,10 @@
 IMPORTANTE, TODOS LOS ALGORITMO EXPONENCIALES, TIRALOS A LA BASURA CONSUMEN MUCHOS RECURSOS, AHORA LOS CUADRATRICOS TODAVIA PUEDEN SERVIR SI TIENEN POCOS INPUTS 
 """
 # Ejemplo de ley de la suma
-
 def f(n):
 
     for i in range(n):
         print(i)
-    
     
     for i in range(n):
         print(i)
@@ -18,37 +16,28 @@ def f(n):
 
 
 # Ejemplo de ley de la suma con exponenciales
-
 def f_2(n):
 
     for i in range(n):
         print(i)
     
-    
     for i in range(n * n):  # Esto se convierte a "n^2"
         print(i) # Este va a ser el mas decisivo por la cantidad de operaciones y el que nos va a interesar
-
 """ O(n) + O(n * n) = O(n + n^2) = O(n^2)"""
 
 
 # Ejemplo de ley de la multiplicacion
-
 def f_3(n):
     # Esto es una iteracion anidada
     for i in range(n):
         for j in range(n): 
             print(i, j)
-
 """ O(n) * O(n) = O(n * n) = O(n^2)"""
 
-#f_3(10)
 
 # Ejemplo de Recursividad multiple
-
 def fibonacci(n): # Cada vez que ejecutamos esta funcion fibonacci, estamos ejecutando otras 2 funciones fibonaccis
     if n == 0 or n == 1:
         return 1
-
     return fibonacci(n - 1) + fibonacci(n - 2)
-
 """O(2^n)"""
