ae030fd11aeec6158f51d1a07b995949051a2f07
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-01-20 23:15:49 -0600

    Mejoras en la explicacion del funcionamiento del algoritmo

--- ordenamiento_burbuja.py
+++ ordenamiento_burbuja.py
@@ -2,10 +2,10 @@ import random
 
 # Funcion que aplica el "Ordenamiento de Burbuja"
 def ordenamiento_de_burbujas(lista):
-    n = len(lista)
+    list_length = len(lista)
 
-    for i in range(n):
-        for j in range(0, n - i - 1):                               # Posicion final: Tamaño de lista - lo que ya recorrimos - 1 (index)
+    for i in range(list_length):
+        for j in range(0, list_length - i - 1):                               # Posicion final: Tamaño de lista - lo que ya recorrimos - 1 (index, off by one)
             #print(i, j)                                            # para ver ciclos anidados
             if lista[j] > lista [j + 1]:                            # Comparamos lugar en "j" con su valor a la derecha "j + 1"
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]     # Realizo el intercambio "Swaping" (valor e index)
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     list_size = int(input('¿De qué tamaño sera la lista? '))    
 
     lista = [random.randint(0, 9) for i in range(list_size)] # Llenamos la lista con numeros aleatoreos
-    print(f'Lista desordenada: \n{lista}  \n {"-" * 25}')
+    print(f'Lista desordenada: \n{lista}  \n{"-" * 25}')
 
     lista_ordenada = ordenamiento_de_burbujas(lista)
     print(f'{"-" * 25}\nLista ordenada: \n{lista_ordenada}')
