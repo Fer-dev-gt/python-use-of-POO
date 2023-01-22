aa2543816626b65d5805d28bc12bc88f1ee5e0d7
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-01-21 23:15:45 -0600

    Mejoras de legibilidad

--- graficado/ejercicio_grafica.py
+++ graficado/ejercicio_grafica.py
@@ -1,14 +1,14 @@
-from bokeh.models import ColumnDataSource                                                           # Importamos diferentes modulos, librerias y funciones
+from bokeh.models import ColumnDataSource                                                                                               # Importamos diferentes modulos, librerias y funciones
 from bokeh.palettes import Bright6
 from bokeh.plotting import figure, show, output_file
 from bokeh.transform import factor_cmap
 
 if __name__ == '__main__':
-    output_file('goles_anotados.html')                                                              # Nombramos el documento output (html)
-    numero_jornada = ['Jornada 1', 'Jornada 2', 'Jornada 3', 'Jornada 4', 'Jornada 5',]             # Eje "x"
-    goles_anotados = [2, 1, 1, 3, 4]                                                                # Eje "y"
+    output_file('goles_anotados.html')                                                                                                  # Nombramos el documento output (html)
+    numero_jornada = ['Jornada 1', 'Jornada 2', 'Jornada 3', 'Jornada 4', 'Jornada 5',]                                                 # Eje "x"
+    goles_anotados = [2, 1, 1, 3, 4]                                                                                                    # Eje "y"
 
-    diccionario = ColumnDataSource(data=dict(numero_jornada=numero_jornada, goles_anotados=goles_anotados))  # Unificamos los datos en un diccionario
+    diccionario = ColumnDataSource(data=dict(numero_jornada=numero_jornada, goles_anotados=goles_anotados))                             # Unificamos los datos en un diccionario
 
     grafica = figure(x_range=numero_jornada, height=350, toolbar_location=None, title="Goles anotados por jornada de Platzi FC")        # Generamos la grafica
                                                                                                                                         # Le agregamos los datos fuente y caracteristicas de como va a mostrar la grafica
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     grafica.legend.orientation = "horizontal"
     grafica.legend.location = "top_center"
 
-show(grafica)                                               # Mostramos la gráfica
+show(grafica)                                                                                                                           # Mostramos la gráfica
